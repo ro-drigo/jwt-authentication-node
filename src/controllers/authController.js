a921cf6e51e640c9e8c0155b9060f3714e955cb3
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-13 15:35:17 -0300

    user login validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 //buscando nossa tabela usuário
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 //Router para definir rotas só para usuários
 const router = express.Router()
@@ -26,6 +27,25 @@ router.post('/register', async (req, res) => {
         return res.status(400).send({ error: 'Registration Failed' })
     
     }
+})
+
+//fazendo a rota de autenticaçao
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    //o +password serve para mostrar a senha, por mais que esteja false no select na estrutura
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user)
+        return res.status(400).send({ error: 'User not found' })
+
+    //precisamos desencriptar a senha que está vindo do bd para comparar
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' })
+
+    user.password = undefined
+    
+    res.send({ user })
 })
 
 //o /auth estará prefixada antes das outras rotas feitas no controller
