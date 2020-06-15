337f413f9c675db53643e1d67adbb009286cc145
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-15 17:51:21 -0300

    creating route post to create project

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/auth')
 
+const Project = require('../models/project')
+const Task = require('../models/task')
+
 const router = express.Router()
 
 //ver se o token está válido
 router.use(authMiddleware)
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId })
+router.get('/', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+//listar um só
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+//rota para criar
+router.post('/', async (req, res) => {
+    try {
+        const project = await Project.create(req.body)
+
+        return res.send({ project })
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error creating new project' })
+    }
+})
+
+//rota para editar
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+//rota para deletar
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId })
 })
 
 module.exports = app => app.use('/projects', router)
